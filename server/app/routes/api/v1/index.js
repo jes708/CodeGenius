affe138b45fff0d655ff48ef574d6d90577388a6
thejohnbackes <jbackes@thisismotive.com>
2016-06-22 23:59:55 -0400

    fsa generator scaffolding, edited for our app. pending approval of tree

---
+++ server/app/routes/api/v1/index.js
@@ -0,0 +1,16 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+
+router.use('/users', require('./users'));
+router.use('/annotations', require('./annotations'));
+router.use('/questions', require('.assessments/questions'));
+router.use('/evaluations', require('.assessments/evaluations'));
+router.use('/assessments', require('.assessments/assessments'));
+
+
+// Make sure this is after all of
+// the registered routes!
+router.use(function (req, res) {
+    res.status(404).end();
+});

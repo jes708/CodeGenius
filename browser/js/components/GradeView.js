37c02af23a61a2e19cfb4f28231bdadd1ac6e4db
Jason Hang <dev.jdhang@gmail.com>
2016-06-25 17:58:58 -0400

    Added more the grader's view, will have to add functionality, just mockups right now.

--- browser/js/components/GradeView.js
+++ browser/js/components/GradeView.js
@@ -6,6 +6,19 @@ import FlatButton from 'material-ui/FlatButton'
 import Paper from 'material-ui/Paper'
 import FontIcon from 'material-ui/FontIcon'
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton'
+
+const SAMPLE_SPEC = {
+  "Fake Library App": [
+    "static files (from the static folder in the public folder) on /files route",
+    "handles internal server errors",
+    "handles custom errors",
+  ],
+  "Fake Library App /api/books": [
+    "GET all",
+    "POST one"
+  ]
+}
 
 const styles = {
   paperStyle: {
@@ -22,7 +35,7 @@ const styles = {
   container: {
     padding: 10
   },
-  grader: {
+  gradingPane: {
     backgroundColor: '#64B5F6'
   },
   skinny: {
@@ -31,37 +44,84 @@ const styles = {
   },
   noTopPadding: {
     paddingTop: 0
+  },
+  infoCard: {
+    backgroundColor: '#1E88E5'
+  },
+  gradingInfo: {
+    color: '#FFF',
+    padding: 16
+  },
+  gradingTitle: {
+    fontSize: 24,
+    fontWeight: '400'
+  },
+  gradingSubtitle: {
+    fontSize: 16,
+    color: '#F5F5F5',
+    fontWeight: '300'
   }
 }
 
 export default class GradeView extends Component {
+  renderCriterion () {
+    return (
+      <RadioButtonGroup>
+        <RadioButton
+          value="1"
+          label="Unbearable"
+        />
+        <RadioButton
+          value="2"
+          label="Horrible"
+        />
+        <RadioButton
+          value="3"
+          label="Acceptable"
+        />
+        <RadioButton
+          value="4"
+          label="Near Perfect"
+        />
+        <RadioButton
+          value="5"
+          label="Flawless"
+        />
+      </RadioButtonGroup>
+    )
+
+  }
+
   renderCards () {
     let cards = []
-    for (let i = 0; i < 5; i++) {
-      cards.push(
-        <Card>
-          <CardHeader
-            title={`Question ${i + 1}`}
-            subtitle={`Question ${i + 1} Details`}
-            actAsExpander={true}
-            showExpandableButton={true}
-          />
-          <CardText expandable={true} style={styles.noTopPadding}>
-            <hr style={styles.skinny} />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions expandable={true}>
-            <FlatButton
-              label='Add Criterion'
-              icon={<FontIcon className='fa fa-plus' />}
+    Object.keys(SAMPLE_SPEC).forEach(title => {
+      SAMPLE_SPEC[title].forEach(spec => {
+        cards.push(
+          <Card style={styles.skinny}>
+            <CardHeader
+              title={title}
+              subtitle={spec}
+              actAsExpander={true}
+              showExpandableButton={true}
             />
-          </CardActions>
-        </Card>
-      )
-    }
+            <CardText expandable={true} style={styles.noTopPadding}>
+              <hr style={styles.skinny} />
+              {this.renderCriterion()}
+            </CardText>
+            <CardActions expandable={true}>
+              <FlatButton
+                label='Add Criterion'
+                icon={<FontIcon className='fa fa-plus' />}
+              />
+              <FlatButton
+                label='Add Comment'
+                icon={<FontIcon className='fa fa-pencil' />}
+              />
+            </CardActions>
+          </Card>
+        )
+      })
+    })
     return cards
   }
 
@@ -76,8 +136,22 @@ export default class GradeView extends Component {
           </Paper>
         </div>
         <div className='col-lg-4'>
-          <Paper zDepth={2} style={Object.assign(styles.grader, styles.paperStyle)}>
+          <Paper zDepth={2} style={Object.assign(styles.gradingPane, styles.paperStyle)}>
             <div style={styles.content}>
+              <Card style={Object.assign(styles.infoCard, styles.skinny)}>
+                <div style={styles.gradingInfo}>
+                  <div style={styles.gradingTitle}>Assessment Title</div>
+                  <div style={styles.gradingSubtitle}>GitHub Repo Link</div>
+                </div>
+                <CardActions>
+                  <FlatButton
+                    label='Next Student'
+                    hoverColor={'#2196F3'}
+                    rippleColor={'#90CAF9'}
+                    style={{color: '#F5F5F5'}}
+                  />
+                </CardActions>
+              </Card>
               <RaisedButton
                 label='Add Question'
                 primary={true}

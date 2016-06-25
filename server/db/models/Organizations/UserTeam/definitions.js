c3ee2600a9fbcd589aa63d1127b5056cbd28b5f8
Jonathan Schwarz <jes708@nyu.edu>
2016-06-25 14:04:10 -0400

    Update definitions.js

--- server/db/models/Organizations/UserTeam/definitions.js
+++ server/db/models/Organizations/UserTeam/definitions.js
@@ -8,7 +8,7 @@ module.exports = function(db){
     return {
       role: {
         type: Sequelize.ENUM,
-        values: ['student', 'teacher']
+        values: ['student', 'instructor']
       }
     }
 }

6915df9a8a51b425cd0f3dca55a82f77b586372a
thejohnbackes <jbackes@thisismotive.com>
2016-06-25 20:54:44 -0400

    made seed work for teams and organizations, including associations. and it is a lot better

--- seed.js
+++ seed.js
@@ -17,41 +17,111 @@ name in the environment files.
 
 */
 
-var chalk = require('chalk');
-var db = require('./server/db');
-var User = db.models['user'];
-var Promise = require('sequelize').Promise;
+var chalk = require( 'chalk' );
+var db = require( './server/db' );
+const User = db.models[ 'user' ];
+const Organization = db.models[ 'organization' ]
+const UserOrganization = db.models[ 'userOrganization' ]
+var Promise = require( 'sequelize' )
+  .Promise;
+const faker = require( 'faker' );
+const utils = require( './server/app/routes/utils' );
+const {
+  Credentials
+} = utils;
+const Sequelize = require( 'sequelize' );
 
-var seedUsers = function () {
+const seedUsers = function ( n = Math.ceil( Math.random() * 100 ) ) {
 
-    var users = [
-        {
-            email: 'testing@fsa.com',
-            password: 'password'
-        },
-        {
-            email: 'obama@gmail.com',
-            password: 'potus'
-        }
-    ];
+  let users = Array
+    .from( {
+        length: n
+      }, credentials =>{
+      credentials = new Credentials(
+        faker.random.word(),
+        'password',
+        faker.internet.exampleEmail()
+      )
+      credentials.username = credentials.username.replace(/\W/g, '').toLowerCase()
+      credentials.photo = faker.internet.avatar();
+      credentials.name = faker.name.findName();
+      return credentials;
+    } )
+  let Omri = new Credentials(
+    'omriBear',
+    'password',
+    'testing@fsa.com'
+  );
+  Omri.isAdmin = true;
+  users.push( Omri );
 
-    var creatingUsers = users.map(function (userObj) {
-        return User.create(userObj);
-    });
+  var creatingUsers = users.map( function ( userObj ) {
+    return User.create( userObj );
+  } );
 
-    return Promise.all(creatingUsers);
+  return Promise.all( creatingUsers );
 
 };
 
-db.sync({ force: true })
-    .then(function () {
-        return seedUsers();
-    })
-    .then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.exit(0);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1);
-    });
+
+
+const seedOrganizations = function ( n = Math.ceil( Math.random() * 100 ) ) {
+  let organizations = User.findAll( {
+      limit: n
+    } )
+    .map( user => {
+      let name = faker.company.companyName();
+      return user.createOrganization( {
+        name
+      }, {
+        role: 'creator'
+      } )
+    } )
+  return organizations;
+}
+
+const seedTeams = function ( n = Math.ceil( Math.random() * 100 ) ) {
+  return Organization.findAll( {
+      // limit: n,
+      include: [ {
+        model: User
+      } ]
+    } )
+    .map( organization => [
+      organization,
+      organization.users[0],
+      faker.helpers.shuffle( faker.commerce.productName().split( ' ' ) ).join( '' )
+    ])
+    .map( result => {
+      let [organization, user, name] = result;
+      return user.createTeam( {
+          name
+        }, {
+          role: 'instructor',
+        } )
+        .then( team => team.setOrganization( organization ) )
+    } )
+}
+
+
+
+db.sync( {
+    force: true
+  } )
+  .then( function () {
+    return seedUsers();
+  } )
+  .then( function () {
+    return seedOrganizations();
+  } )
+  .then( function () {
+    return seedTeams();
+  } )
+  .then( function () {
+    console.log( chalk.green( 'Seed successful!' ) );
+    process.exit( 0 );
+  } )
+  .catch( function ( err ) {
+    console.error( err );
+    process.exit( 1 );
+  } );

04f827a60d04a66995ee79200b834aec832e717b
thejohnbackes <jbackes@thisismotive.com>
2016-06-28 18:59:24 -0400

    more cleanup

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,15 +1,15 @@
 'use strict';
-var router = require('express').Router();
-const utils = require('./utils');
+var router = require( 'express' ).Router();
+const utils = require( './utils' );
 // const respond = util.responder(router);
-const {respondWith404, bindRouterToUse} = utils
-const routerUse = bindRouterToUse(router);
+const { respondWith404, bindRouterToUse } = utils;
+const routerUse = bindRouterToUse( router );
 
 module.exports = router;
 
 
-routerUse('/', './api')
+routerUse( '/', './api' );
 
 // Make sure this is after all of
 // the registered routes!
-respondWith404(router);
+respondWith404( router );

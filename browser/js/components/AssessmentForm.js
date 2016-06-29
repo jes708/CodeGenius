1dbed0f0090f55b95f953922a5cbb221440c793c
Jason Hang <dev.jdhang@gmail.com>
2016-06-29 18:53:28 -0400

    Creating AssessmentForm

---
+++ browser/js/components/AssessmentForm.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import Formsy from 'formsy-react'
+import { FormsyText } from 'formsy-material-ui/lib'
+
+class AssessmentForm extends Component {
+
+}
+        // <Formsy.Form
+        //   ref='form'
+        //   onValid={this.enableButton.bind(this)}
+        //   onInvalid={this.disableButton.bind(this)}
+        //   onValidSubmit={this._submitForm.bind(this)}>
+        //   <FormsyText
+        //     name="email"
+        //     type="email"
+        //     required
+        //     floatingLabelText="Email"
+        //     validations='isEmail'
+        //   />
+        //   <FormsyText
+        //     name="password"
+        //     type="password"
+        //     required
+        //     floatingLabelText="Password"
+        //   />
+        //   <div style={Object.assign({}, styles.button, styles.fullWidth)}>
+        //     <RaisedButton
+        //       type="submit"
+        //       label='Login'
+        //       secondary={true}
+        //       style={styles.fullWidth}
+        //       disabled={!this.state.canSubmit}
+        //     />
+        //   </div>
+        // </Formsy.Form>

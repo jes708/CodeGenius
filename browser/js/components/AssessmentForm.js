2581ae8fe4b800ccbd5ec1f5c7cf7f2fe24d97b2
Jason Hang <dev.jdhang@gmail.com>
2016-06-29 19:07:54 -0400

    Need to switch back to jansen's broken branch.

--- browser/js/components/AssessmentForm.js
+++ browser/js/components/AssessmentForm.js
@@ -2,37 +2,22 @@
 
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import Formsy from 'formsy-react'
-import { FormsyText } from 'formsy-material-ui/lib'
+import { reduxForm } from 'redux-form'
+import Paper from 'material-ui/Paper'
+export const fields = ['name', 'description', 'repoUrl']
 
 class AssessmentForm extends Component {
-
+  render () {
+    return (
+      <Paper>
+        <form>
+        </form>
+      </Paper>
+    )
+  }
 }
-        // <Formsy.Form
-        //   ref='form'
-        //   onValid={this.enableButton.bind(this)}
-        //   onInvalid={this.disableButton.bind(this)}
-        //   onValidSubmit={this._submitForm.bind(this)}>
-        //   <FormsyText
-        //     name="email"
-        //     type="email"
-        //     required
-        //     floatingLabelText="Email"
-        //     validations='isEmail'
-        //   />
-        //   <FormsyText
-        //     name="password"
-        //     type="password"
-        //     required
-        //     floatingLabelText="Password"
-        //   />
-        //   <div style={Object.assign({}, styles.button, styles.fullWidth)}>
-        //     <RaisedButton
-        //       type="submit"
-        //       label='Login'
-        //       secondary={true}
-        //       style={styles.fullWidth}
-        //       disabled={!this.state.canSubmit}
-        //     />
-        //   </div>
-        // </Formsy.Form>
+
+export default reduxForm({
+  form: 'form',
+  fields
+})(AssessmentForm)

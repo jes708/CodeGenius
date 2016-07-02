5d4159daee8b3b265558c1b2619854c8a69f9c5e
Jason Hang <dev.jdhang@gmail.com>
2016-07-02 10:00:26 -0400

    Added ability to use process.env.NODE_ENV on our front end code, via webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
@@ -24,6 +25,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("style.css", {allChunks: true})
+    new ExtractTextPlugin("style.css", {allChunks: true}),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
   ]
 }

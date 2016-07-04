8d05d17031faf7f65d5f1fded67b6ece31668026
Jason Hang <dev.jdhang@gmail.com>
2016-07-04 14:45:11 -0400

    Fixed async issue with fetching github emails.

--- server/app/configure/authentication/github.js
+++ server/app/configure/authentication/github.js
@@ -7,86 +7,85 @@ var Promise = require('bluebird');
 
 module.exports = function(app, db) {
 
-    var User = db.model('user');
+  var User = db.model('user');
 
-    var githubConfig = app.getValue('env').GITHUB;
+  var githubConfig = app.getValue('env').GITHUB;
 
-    var githubCredentials = {
-        clientID: githubConfig.clientID,
-        clientSecret: githubConfig.clientSecret,
-        callbackURL: githubConfig.callbackURL
-    };
+  var githubCredentials = {
+    clientID: githubConfig.clientID,
+    clientSecret: githubConfig.clientSecret,
+    callbackURL: githubConfig.callbackURL
+  };
 
-    var verifyCallback = function(accessToken, refreshToken, profile, done) {
-        GitHub.authenticate({
-            type: 'oauth',
-            token: accessToken
-        })
+  var verifyCallback = function(accessToken, refreshToken, profile, done) {
+    GitHub.authenticate({
+      type: 'oauth',
+      token: accessToken
+    })
 
-        User.findOne({
-                where: {
-                    username: profile.username
-                }
-            })
-            .then(function(user) {
-                var getEmails = GitHub.users.getEmails({
-                    per_page: 100
-                })
-                if (user) {
-                    var userUpdate = user.update({
-                        github_id: profile.id,
-                        name: profile.displayName,
-                        email: profile.email ? profile.email.value : [profile.username, 'no-email.com'].join('@'),
-                        photo: profile.photos[0].value,
-                        github_token: accessToken
-                    });
-                    return Promise.all([userUpdate, getEmails])
-                } else {
-                    var userCreate = User.create({
-                        github_id: profile.id,
-                        name: profile.displayName,
-                        username: profile.username,
-                        email: profile.email ? profile.email.value : [profile.username, 'no-email.com'].join('@'),
-                        photo: profile.photos[0].value,
-                        github_token: accessToken
-                    });
-                    return Promise.all([userCreate, getEmails])
-                }
-            })
-            .spread((user, emails) => {
-                if (emails) {
-                    let primaryEmail = emails.filter(email => {
-                        return email.primary === true
-                    })
-                    return user.update({
-                        email: primaryEmail[0].email
-                    })
-                } else {
-                    return user
-                }
-            })
-            .then(function(userToLogin) {
-                done(null, userToLogin);
-            })
-            .catch(function(err) {
-                console.error('Error creating user from Github authentication', err);
-                done(err);
-            });
-    };
+    User.findOne({
+        where: {
+          username: profile.username
+        }
+      })
+      .then(function(user) {
+        var getEmails = GitHub.users.getEmailsAsync({ per_page: 100 })
+
+        if (user) {
+          var userUpdate = user.update({
+            github_id: profile.id,
+            name: profile.displayName,
+            email: profile.email ? profile.email.value : [profile.username, 'no-email.com'].join('@'),
+            photo: profile.photos[0].value,
+            github_token: accessToken
+          });
+          return Promise.all([userUpdate, getEmails])
+        } else {
+          var userCreate = User.create({
+            github_id: profile.id,
+            name: profile.displayName,
+            username: profile.username,
+            email: profile.email ? profile.email.value : [profile.username, 'no-email.com'].join('@'),
+            photo: profile.photos[0].value,
+            github_token: accessToken
+          });
+          return Promise.all([userCreate, getEmails])
+        }
+      })
+      .spread((user, emails) => {
+        if (emails) {
+          let primaryEmail = emails.filter(email => {
+            return email.primary === true
+          })
+          return user.update({
+            email: primaryEmail[0].email
+          })
+        } else {
+          return user
+        }
+      })
+      .then(function(userToLogin) {
+        done(null, userToLogin);
+      })
+      .catch(function(err) {
+        console.error('Error creating user from Github authentication', err);
+        done(err);
+      });
+  };
 
 
-    passport.use(new GitHubStrategy(githubCredentials, verifyCallback));
+  passport.use(new GitHubStrategy(githubCredentials, verifyCallback));
 
-    app.get('/auth/github', passport.authenticate('github', {
-        scope: 'repo gist user:email read:org'
-    }));
+  app.get('/auth/github', passport.authenticate('github', {
+    scope: 'repo gist user:email read:org'
+  }));
 
-    app.get('/auth/github/callback',
-        passport.authenticate('github', {
-            failureRedirect: '/login'
-        }),
-        function(req, res) {
-            res.redirect('/');
-        });
+  app.get('/auth/github/callback',
+    passport.authenticate('github', {
+      failureRedirect: '/login'
+    }),
+    function(req, res) {
+      res.redirect('/');
+    });
 
 };

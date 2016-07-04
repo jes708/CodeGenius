87c4a16d6caadce9646e4c1bf4c0535273b58522
jes708 <jonschwarz@nyu.edu>
2016-07-04 18:45:00 -0400

    homepage styling

--- browser/js/containers/Home/index.js
+++ browser/js/containers/Home/index.js
@@ -1,16 +1,18 @@
 'use strict'
 
 import React, { Component } from 'react'
-import GitHubButton from '../../shared/GitHubButton'
+import GitHubInvert from '../../shared/GitHubInvert'
 import Paper from 'material-ui/Paper'
 import { blue300, blue600 } from 'material-ui/styles/colors'
 
 const styles = {
   header: {
     textAlign: 'center',
-    fontSize: '60px',
+    fontSize: '100px',
     color: '#fdfdfd',
-    fontWeight: '300'
+    fontWeight: 900,
+    fontFamily: 'Helvetica Neue',
+    textShadow: '0px 0px 10px black'
   },
   paperHeader: {
     backgroundColor: blue600,
@@ -18,23 +20,41 @@ const styles = {
     marginBottom:'60px'
   },
   paperStyle: {
-    backgroundColor: blue300,
+    backgroundImage: "url('http://i.imgur.com/3GPNYHn.jpg')",
+    backgroundSize: '100%',
     width: '100%',
     height: 'calc(100vh - 56px)',
     textAlign: 'center',
     padding: '25vh',
     paddingTop: '15vh',
     paddingBottom: '20vh',
+  },
+  button: {
+    boxShadow: '0px 0px 10px black',
+  },
+  hr: {
+    marginTop: 0,
+    border: '0',
+    height: '1px',
+    width: '50%',
+    backgroundImage: 'linear-gradient(to right, transparent, white, transparent)'
+  },
+  p: {
+    fontSize: '23px',
+    color: 'white',
+    fontWeight: 100,
+    letterSpacing: '1px',
+    marginBottom: '20px'
   }
 }
 
 export default function Home () {
   return (
     <div style={styles.paperStyle}>
-      <Paper zDepth={3} style={styles.paperHeader}>
-        <div style={styles.header}>Welcome to CodeGenius!</div>
-      </Paper>
-      <GitHubButton href='/auth/github' />
+        <div style={styles.header}>CodeGenius</div>
+        <hr style={styles.hr} />
+        <p style={styles.p}>CodeGenius optimizes the process of evaluating student code assessments<br />so you can get all of your grading done in one sitting.</p>
+      <GitHubInvert style={styles.button} href='/auth/github' />
     </div>
   )
 }

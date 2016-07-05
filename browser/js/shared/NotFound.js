acc596b8cdb63996425a048a9e480aa1acf60c5e
Jason Hang <dev.jdhang@gmail.com>
2016-07-05 16:22:23 -0400

    Redirect unauthenticated users to always go to the home screen. Authenticated users will never see the home screen, it defaults to the Grade view. If they go to other links, they will still work though.

---
+++ browser/js/shared/NotFound.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+const NotFound = (props) => {
+  return (
+    <div style={{textAlign: 'center', marginTop: '100px'}}>
+      <h1>404</h1>
+      <h3>Page Not Found</h3>
+    </div>
+  )
+}
+
+export default NotFound

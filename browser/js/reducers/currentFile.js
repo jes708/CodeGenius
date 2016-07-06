d44d283e858d3ffc3de8aa915a3db4c750d965f4
Jason Hang <dev.jdhang@gmail.com>
2016-07-06 15:49:25 -0400

    Reformatted the currentFile state tree.

--- browser/js/reducers/currentFile.js
+++ browser/js/reducers/currentFile.js
@@ -6,7 +6,12 @@ const currentFile = (state = {}, action) => {
   switch (action.type) {
     case FETCH_REPO_SUCCESS:
       const nextState = Object.assign({}, state)
+      let splitPath = action.path.split('/')
       nextState.path = action.path
+      nextState.user = splitPath[0]
+      nextState.repo = splitPath[1]
+      nextState.basePath = `${splitPath[0]}/${splitPath[1]}`
+      nextState.filePath = `${splitPath.slice(2).join('/')}`
       return nextState
     default:
       return state

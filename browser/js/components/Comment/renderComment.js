8b08d1c77524fcb8be63d89194e6d03344f079df
thejohnbackes <jbackes@thisismotive.com>
2016-07-07 15:35:16 -0400

    score is working and persists

--- browser/js/components/Comment/renderComment.js
+++ browser/js/components/Comment/renderComment.js
@@ -4,6 +4,7 @@ import styles from '../graderStyles';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Tags} from '../../containers/Tag';
 import MarkdownWrapper from '../../containers/Markdown';
+import Slider from 'material-ui/Slider';
 import FlatButton from 'material-ui/FlatButton'
 import Dialog from 'material-ui/Dialog';
 
@@ -26,6 +27,7 @@ export default class RenderComment extends Component {
     this.renderComment = this.renderComment.bind(this);
     this.handleSubmitDialog = this.handleSubmitDialog.bind(this);
     this.renderMarkdown = this.renderMarkdown.bind(this);
+    this.renderScore = this.renderScore.bind(this);
   }
   componentWillReceiveProps(nextProps){
     let {contents, isEditing} = nextProps;
@@ -53,6 +55,41 @@ export default class RenderComment extends Component {
         dialog: nextDialog
       })
     }
+  }
+  handleSlider(event, value){
+    this.updateContents({score: value});
+  }
+  renderScore(){
+    let {contents, isEditing} = this.state;
+    let buttonStyle = styles.assessmentButtons;
+    return (
+      <div>
+        <p>
+          {(contents.score) ? (<span>Score: {contents.score}</span>) : null }
+        </p>
+        <div>
+        {(
+          isEditing ? (
+            <span>
+              <p>Set a score for this comment</p>
+              <span>0</span>
+              <Slider
+              max={5}
+              min={0}
+              step={0.5}
+              defaultValue={contents.score || 1}
+              value={contents.score}
+              onChange={
+                this.handleSlider.bind(this)
+              }
+              />
+              <span>5</span>
+            </span>
+          ) : (null)
+        )}
+        </div>
+      </div>
+    )
   }
   renderMarkdown(){
     let {contents, isEditing} = this.state;
@@ -114,18 +151,19 @@ export default function renderComment () {
     let {contents, isEditing} = this.state;
     let buttonStyle = styles.assessmentButtons;
     let id = 0;
-    return ( <div>
+    return (
+      <div>
         <span key={id++} >
           {this.renderMarkdown()}
         </span>
         <span key={id++}>
-      { (!contents.score && isEditing) ? <RaisedButton style={buttonStyle} label="Add Score" /> : ""}
+          {this.renderScore()}
         </span>
         <span key={id++}>
-      {/* (!contents.solutionCodeLink && isEditing) ? <RaisedButton style={buttonStyle} label="Add Solution Code" /> : "" */}
+          {/* (!contents.solutionCodeLink && isEditing) ? <RaisedButton style={buttonStyle} label="Add Solution Code" /> : "" */}
         </span>
         <span key={id++}>
-      { (!contents.tags && isEditing) ? <RaisedButton style={buttonStyle} label="Add Tag" /> : (
+          { (!contents.tags && isEditing) ? <RaisedButton style={buttonStyle} label="Add Tag" /> : (
         <div>
           <Tags tags={contents.tags} isEditing={isEditing} />
         </div>
@@ -133,14 +171,13 @@ export default function renderComment () {
       }
         </span>
         <span key={id++}>
-      {/* (!contents.attachments && isEditing) ? <RaisedButton style={buttonStyle} label="Add Attachment" /> : ""*/}
+          {/* (!contents.attachments && isEditing) ? <RaisedButton style={buttonStyle} label="Add Attachment" /> : ""*/}
         </span>
         <span key={id++}>
-      { (!contents.selection) ? (isEditing ? <RaisedButton style={buttonStyle} label="Add Annotation" /> : "") : (
+          { (!contents.selection) ? (isEditing ? <RaisedButton style={buttonStyle} label="Add Annotation" /> : "") : (
         <div>
           <div>
             <pre>
-              {/*contents.selection.toString()*/}
               {contents.annotation && contents.annotation.selectionString ? contents.annotation.selectionString : null}
             </pre>
           </div>

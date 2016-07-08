74c13abd60d0282af675974e1a40f3a03fd8e405
Jason Hang <dev.jdhang@gmail.com>
2016-07-08 19:09:24 -0400

    Reverting...

--- server/db/_db.js
+++ server/db/_db.js
@@ -5,14 +5,7 @@ var Sequelize = require('sequelize');
 
 global._dbPath = path.join(__dirname, "_db.js");
 var env = require(path.join(__dirname, '../env'));
-var match = process.env.DATABASE_URI.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
-var db = new Sequelize(env.DATABASE_URI, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  port: match[4],
-  host: match[3],
-  logging: env.LOGGING
-});
+var db = new Sequelize(env.DATABASE_URI, { logging: env.LOGGING })
 
 // global._dbPath = path.join(__dirname, "../server/db/_db.js")
 

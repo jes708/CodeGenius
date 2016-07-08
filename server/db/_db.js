696745305fefb8f506dcb18172efef7169271470
Jason Hang <dev.jdhang@gmail.com>
2016-07-08 18:59:38 -0400

    Sequelize no associations. :(

--- server/db/_db.js
+++ server/db/_db.js
@@ -5,7 +5,14 @@ var Sequelize = require('sequelize');
 
 global._dbPath = path.join(__dirname, "_db.js");
 var env = require(path.join(__dirname, '../env'));
-var db = new Sequelize(env.DATABASE_URI, { logging: env.LOGGING });
+var match = process.env.DATABASE_URI.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+var db = new Sequelize(env.DATABASE_URI, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  port: match[4],
+  host: match[3],
+  logging: env.LOGGING
+});
 
 // global._dbPath = path.join(__dirname, "../server/db/_db.js")
 
